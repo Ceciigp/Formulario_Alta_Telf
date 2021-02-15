319ce2913293fe124901befab8be80876eaa1f50
Ceciigp <78142755+Ceciigp@users.noreply.github.com>
2021-02-15 16:19:34 +0100

    modificado el ts y modificado selectores de scss

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
+// Boton valoracion
 var valoracionIn = document.querySelector('#valoracion');
 var btnvaloracion = document.querySelector('#btnvaloracion');
+// Boton cuenta
 var paisIn = document.querySelector('#pais');
 var ibanIn = document.querySelector('#iban');
 var entidadIn = document.querySelector('#entidad');
@@ -8,9 +10,15 @@ var sucursalIn = document.querySelector('#sucursal');
 var dcIn = document.querySelector('#dc');
 var cuentaIn = document.querySelector('#cuenta');
 var btncuenta = document.querySelector('#btncuenta');
+// Boton fecha
+var fechaIn = document.querySelector('#fecha');
+var btnsemana = document.querySelector('#btnsemana');
 btnvaloracion.addEventListener('click', function () {
     alert("Has valorado con " + valoracionIn.value + " puntos");
 });
 btncuenta.addEventListener('click', function () {
-    alert("Le informamos que su cuenta bancaria es: " + (paisIn.value + ibanIn.value + entidadIn.value + sucursalIn.value + dcIn.value + cuentaIn.value) + " ");
+    alert("Le informamos que su cuenta bancaria es: " + (paisIn.value + "-" + ibanIn.value + "-" + entidadIn.value + "-" + sucursalIn.value + "-" + dcIn.value + "-" + cuentaIn.value) + " ");
+});
+btnsemana.addEventListener('click', function () {
+    alert("La fecha seleccionada en el elemento de fecha es " + fechaIn.value + " ");
 });
--- ts/app.ts
+++ ts/app.ts
@@ -1,9 +1,10 @@
 'use strict'
 
+// Boton valoracion
 const valoracionIn  = (document.querySelector('#valoracion')  as HTMLFormElement)
 const btnvaloracion = (document.querySelector('#btnvaloracion') as HTMLButtonElement)
 
-
+// Boton cuenta
 const paisIn  = (document.querySelector('#pais')  as HTMLFormElement)
 const ibanIn  = (document.querySelector('#iban')  as HTMLFormElement)
 const entidadIn  = (document.querySelector('#entidad')  as HTMLFormElement)
@@ -12,11 +13,20 @@ const dcIn  = (document.querySelector('#dc')  as HTMLFormElement)
 const cuentaIn  = (document.querySelector('#cuenta')  as HTMLFormElement)
 const btncuenta = (document.querySelector('#btncuenta') as HTMLButtonElement)
 
+// Boton fecha
+const fechaIn = (document.querySelector('#fecha')  as HTMLFormElement)
+const btnsemana = (document.querySelector('#btnsemana') as HTMLButtonElement)
 
-btnvaloracion.addEventListener('click',()=>{
+
+
+btnvaloracion.addEventListener('click',(): void =>{
     alert(`Has valorado con ${ valoracionIn.value } puntos`)
 })
 
-btncuenta.addEventListener('click',()=>{
-    alert(`Le informamos que su cuenta bancaria es: ${ paisIn.value+ibanIn.value+entidadIn.value+sucursalIn.value+dcIn.value+cuentaIn.value } `)
+btncuenta.addEventListener('click',(): void =>{
+    alert(`Le informamos que su cuenta bancaria es: ${ paisIn.value+ `-` +ibanIn.value+ `-` +entidadIn.value+ `-` +sucursalIn.value+ `-` +dcIn.value+ `-` +cuentaIn.value } `)
+})
+
+btnsemana.addEventListener('click',(): void =>{
+    alert(`La fecha seleccionada en el elemento de fecha es ${ fechaIn.value } `)
 })
